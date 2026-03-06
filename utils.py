99554b34527a123627b65729fb3366492f32ea00
siddharth7actowiz <siddharth7.actowiz@gmail.com>
2026-03-06 13:50:56 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,10 +1,18 @@
+import os
+import gzip
 import json
 
-def read_file(file_path):
-    try:
-            with open(file_path,"r")as f:
-                python_data=json.load(f)
-                return  python_data
-    except Exception as e:
 
-         print("Error:",read_file.__name__,e)    
+def load_files(data_dir, start, end):
+
+    files = os.listdir(data_dir)
+    files.sort()
+
+    for file in files[start:end]:
+        path = os.path.join(data_dir, file)
+        try:
+            with gzip.open(path, "rt", encoding="utf-8") as f:
+                yield json.load(f)
+
+        except Exception as e:
+            print("Error in func:", load_files.__name__, '\nError: ', e)
