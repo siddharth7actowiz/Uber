bbf1518ebc5c81d4ddb1f314b381125f664c658e
siddharth7actowiz <siddharth7.actowiz@gmail.com>
2026-03-06 13:50:27 +0530

    Validator Class to validate Data

--- validation.py
+++ validation.py
@@ -4,6 +4,7 @@ from typing import Optional
 
 class Restaurant(BaseModel):
     Restaurant_Name: str
+    Res_Id:Optional[str]=None
     Phone_No: Optional[str]
     Full_Address: Optional[str]
     Street_address: Optional[str]
@@ -19,3 +20,10 @@ class Restaurant(BaseModel):
     Category: str
     Featured_Category: str
     Currency: Optional[str]
+    
+    @field_validator("Pincode", mode="before")
+    @classmethod
+    def clean_pincode(cls, v):
+        if v is None or v == "None":
+            return None
+        return str(v)
